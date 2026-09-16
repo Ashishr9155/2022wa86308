57f7c921859406c135c3d1eb128254b74b267a20
Ashishr9155 <97353279+Ashishr9155@users.noreply.github.com>
2026-09-16 15:23:37 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("BITS ID: 2022WA86308");
-        System.out.println("2022WA86308 - Continuous Integration Successful");
+        System.out.println("Jenkins CI /CD Test");
     }
 }
  
