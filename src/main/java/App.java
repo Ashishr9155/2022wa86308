465c7aab3153fc1cd1ca00d352ad9035850db52f
Ashishr9155 <97353279+Ashishr9155@users.noreply.github.com>
2026-09-16 15:09:12 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("BITS ID: 2022WA86308");
-        System.out.println("Jenkins Maven Build Successful");
+        System.out.println("2022WA86308 - Continuous Integration Successful");
     }
 }
  
