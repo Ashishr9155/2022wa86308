9bd41bdd52c13a85ad46122a66a44606b34dc1be
Ashishr9155 <97353279+Ashishr9155@users.noreply.github.com>
2026-09-16 14:45:30 +0530

    Create App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("BITS ID: 2022WA86308");
+        System.out.println("Jenkins Maven Build Successful");
+    }
+}
+ 
